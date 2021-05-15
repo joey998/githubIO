4fd4781d62c2a0913112167b8764f711864a430a
jiaming zhang <2273243363@qq.com>
2021-05-15 23:30:09 +0800

    add something

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:joey998/joey998.github.io.git main
+git push -f git@github.com:joey998/joey998.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
@@ -30,7 +30,7 @@ git push -f git@github.com:joey998/joey998.github.io.git main
 # cd -
 
 # 发布到gitee，因为阿里云下载不了github的代码，只能从gitee下了
-git push -f git@gitee.com:joey998/joey998.gitee.io.git main
+git push -f git@gitee.com:joey998/joey998.gitee.io.git master
 
 # ssh不支持输入密码，所以只能够先把公钥放服务器来进行免密操作
 ssh root@121.42.12.48 << eof
